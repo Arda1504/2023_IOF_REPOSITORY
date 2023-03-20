4ecebb31a3cfae59bfa0dcf094199d79de963bc3
bigmanjr <hlister@gmu.edu>
2023-03-20 14:33:39 -0400

    Hammer and Flame Hammer only appear if you have the items

--- IOFProject/Assets/Scripts/CombatCanvasScript.cs
+++ IOFProject/Assets/Scripts/CombatCanvasScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CombatCanvasScript : MonoBehaviour
 {
+    public GameObject HammerButton;
+    public GameObject FlameHammerButton;
+    public playermovement plymvt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        plymvt = GameObject.Find("player 1").GetComponent<playermovement>();
     }
 
     // Update is called once per frame
@@ -15,4 +18,32 @@ public class CombatCanvasScript : MonoBehaviour
     {
         
     }
+
+    public void ShowAttacks()
+    {
+        if(plymvt.HasHammer == false)
+        {
+            HammerButton.gameObject.SetActive(false);
+
+            FlameHammerButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            HammerButton.gameObject.SetActive(true);
+
+            FlameHammerButton.gameObject.SetActive(true);
+        }
+
+        if(plymvt.HasLamp == false)
+        {
+            
+            FlameHammerButton.gameObject.SetActive(false);
+        }
+        else if(plymvt.HasLamp == true && plymvt.HasHammer == true)
+        {
+            FlameHammerButton.gameObject.SetActive(true);
+        }
+
+    }
+
 }
