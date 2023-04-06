aaad1936b9547c61f00b8928e162607ad4a5c9e4
bigmanjr <hlister@gmu.edu>
2023-04-05 21:21:32 -0400

    Added warning screen before going to floki

---
+++ IOFProject/Assets/Scripts/Warningtriggerscrpt.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Warningtriggerscrpt : MonoBehaviour
+{
+
+    public GameObject Screen;
+    public TextMeshProUGUI ScreenText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Screen.gameObject.SetActive(true);
+            ScreenText.text = "You won't be able to return if you go to Floki. Make sure you have what you need";
+            Destroy(gameObject);
+        }
+    }
+}
